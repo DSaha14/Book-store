e09cd6c156653c32ea303d2c13304afe71a1ebec
DSaha14 <dishasaha1404@gmail.com>
2024-09-11 18:56:21 +0530

    initial commit

---
+++ Frontend/src/components/Freebook.jsx
@@ -0,0 +1,69 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react';
+
+import "slick-carousel/slick/slick.css";
+import "slick-carousel/slick/slick-theme.css";
+import Slider from "react-slick";
+import Cards from './Cards';
+
+
+import list from "../../public/list.json";
+
+function Freebook() {
+    const filterData = list.filter((data) => data.category === "Free");
+    var settings = {
+        dots: true,
+        infinite: false,
+        speed: 500,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 0,
+        responsive: [
+          {
+            breakpoint: 1024,
+            settings: {
+              slidesToShow: 3,
+              slidesToScroll: 3,
+              infinite: true,
+              dots: true
+            }
+          },
+          {
+            breakpoint: 600,
+            settings: {
+              slidesToShow: 2,
+              slidesToScroll: 2,
+              initialSlide: 2
+            }
+          },
+          {
+            breakpoint: 480,
+            settings: {
+              slidesToShow: 1,
+              slidesToScroll: 1
+            }
+          }
+        ]
+      };
+  return (
+    <>
+    <div className="max-w-screen-2x1 container mx-auto md:px-20 px-4">
+        <div>
+        <h1 className="mt-6 font-semibold text-x1 pb-2">Free Offered Books</h1>
+        <p>
+            Enjoy some exciting and free contents. 
+        </p>
+        </div>
+    <div>
+    <Slider {...settings}>
+        {filterData.map((item)=>(
+          <Cards item={item} key={item.id} />
+        ))}
+      </Slider>
+    </div>
+    </div>
+    </>
+  )
+}
+
+export default Freebook
---
+++ Frontend/src/components/courses/Courses.jsx
@@ -0,0 +1,19 @@
+// eslint-disable-next-line no-unused-vars
+import React from 'react'
+import Navbar from '../Navbar'
+import Course from '../Course'
+import Footer from '../Footer'
+
+function Courses() {
+  return (
+    <>
+    <Navbar />
+    <div className="min-h-screen ">
+        <Course />
+    </div>
+    <Footer />
+    </>
+  )
+}
+
+export default Courses
